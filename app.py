22a9e6129901e7a2e9856c3960b5f99a661c9617
Rishabh Jain <rishabh32jain@gmail.com>
2024-02-21 22:02:06 -0500

    committing preliminary cas code... cas no longer works with localhost it seems

---
+++ app.py
@@ -0,0 +1,21 @@
+# Based on https://github.com/shannon-heh/TigerSnatch/blob/main/app.py#L75
+from sys import path
+path.append('src')
+
+from flask import Flask
+from CASClient import CASClient
+
+app = Flask(__name__)
+_cas = CASClient()
+
+
+@app.route('/')
+def hello_world():
+    print("HERE!")
+    netid = _cas.authenticate()
+    print(netid)
+    netid = netid.rstrip()
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000, debug=True)
